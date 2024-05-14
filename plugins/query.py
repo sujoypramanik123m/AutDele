629ec152fd3e94dfc5c22890b2a48eccaf3d066c
Snowball <162016877+Snowball-01@users.noreply.github.com>
2024-05-14 19:38:12 +0530

    Create query.py

---
+++ plugins/query.py
@@ -0,0 +1,104 @@
+import shutil
+import time
+from pyrogram import Client
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from config import Config, Txt
+from helper.database import db
+import random
+import psutil
+from helper.utils import humanbytes
+
+
+@Client.on_callback_query()
+async def cb_handler(client, query: CallbackQuery):
+    data = query.data
+    if data == "start":
+        await query.message.edit_media(
+            InputMediaPhoto(
+                random.choice(Config.PICS),
+                Txt.START_TXT.format(query.from_user.mention),
+
+            ),
+
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton(
+                    '⛅ Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/Kdramaland'),
+                InlineKeyboardButton(
+                    '🌨️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/SnowDevs')
+            ], [
+                InlineKeyboardButton('❄️ Aʙᴏᴜᴛ', callback_data='about'),
+                InlineKeyboardButton('❗ Hᴇʟᴩ', callback_data='help')
+            ]])
+        )
+    elif data == "help":
+
+        await query.message.edit_media(
+            InputMediaPhoto(
+                random.choice(Config.PICS),
+                Txt.HELP_TXT
+
+            ),
+
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton("✘ ᴄʟᴏsᴇ", callback_data="close"),
+                InlineKeyboardButton("⟪ ʙᴀᴄᴋ", callback_data="start")
+            ]])
+        )
+    elif data == "about":
+        await query.message.edit_media(
+            InputMediaPhoto(
+                random.choice(Config.PICS),
+                Txt.ABOUT_TXT.format(client.mention),
+
+            ),
+
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton("✘ ᴄʟᴏsᴇ", callback_data="close"),
+                InlineKeyboardButton("⟪ ʙᴀᴄᴋ", callback_data="start")
+            ]])
+        )
+
+    elif data == 'stats':
+        buttons = [[InlineKeyboardButton(
+            '• ʙᴀᴄᴋ', callback_data='start'), InlineKeyboardButton('⟲ ʀᴇʟᴏᴀᴅ', callback_data='stats')]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        currentTime = time.strftime("%Hh%Mm%Ss", time.gmtime(
+            time.time() - Config.BOT_UPTIME))
+        total, used, free = shutil.disk_usage(".")
+        total = humanbytes(total)
+        used = humanbytes(used)
+        free = humanbytes(free)
+        cpu_usage = psutil.cpu_percent()
+        ram_usage = psutil.virtual_memory().percent
+        disk_usage = psutil.disk_usage('/').percent
+        await query.message.edit_media(
+            InputMediaPhoto(
+                random.choice(Config.PICS),
+                Txt.STATS_TXT.format(
+                    currentTime, total, used, disk_usage, free, cpu_usage, ram_usage)
+            ),
+            reply_markup=reply_markup
+        )
+
+    elif data == 'userbot':
+        userBot = await db.get_user_bot(query.from_user.id)
+
+        text = f"Name: {userBot['name']}\nUserName: @{userBot['username']}\n UserId: {userBot['user_id']}"
+
+        await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('❌ ʀᴇᴍᴏᴠᴇ ❌', callback_data='rmuserbot')], [InlineKeyboardButton('✘ ᴄʟᴏsᴇ ✘', callback_data='close')]]))
+
+    elif data == 'rmuserbot':
+        try:
+            await db.remove_user_bot(query.from_user.id)
+            await query.message.edit(text='**User Bot Removed Successfully ✅**', reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('✘ ᴄʟᴏsᴇ ✘', callback_data='close')]]))
+        except:
+            await query.answer(f'Hey {query.from_user.first_name}\n\n You have already deleted the user')
+
+    elif data == "close":
+        try:
+            await query.message.delete()
+            await query.message.reply_to_message.delete()
+            await query.message.continue_propagation()
+        except:
+            await query.message.delete()
+            await query.message.continue_propagation()
