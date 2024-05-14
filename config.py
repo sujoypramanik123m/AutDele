6a9f494aedcfdefcdcebfb27ce1ba92ed7454911
Snowball <162016877+Snowball-01@users.noreply.github.com>
2024-05-14 19:48:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # ⚠️ Required
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "Sigma_Rename")
     DB_URL = os.environ.get("DB_URL", "")  # ⚠️ Required
 
     # other configs
