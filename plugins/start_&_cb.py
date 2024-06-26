54e375f19993302d0a2e5239c269b632b16d3aae
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-26 18:31:41 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMedi
 from helper.database import db
 from config import Config, Txt
 from info import AUTH_CHANNEL
-from syd import is_req_subscribed
+from helper.utlis import is_req_subscribed
 import humanize
 from time import sleep
 
