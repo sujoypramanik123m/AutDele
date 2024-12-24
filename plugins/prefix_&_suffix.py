5ddaa8eaf23ae88b4cc6bacc5fe54e2773063ba5
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-12-24 08:48:50 +0530

    Update prefix_&_suffix.py

--- plugins/prefix_&_suffix.py
+++ plugins/prefix_&_suffix.py
@@ -67,3 +67,43 @@ async def see_csuffix(client, message):
         await SnowDev.edit(f"**ʏᴏᴜʀ ꜱᴜꜰꜰɪx:-**\n\n`{suffix}`")
     else:
         await SnowDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+
+@Client.on_message(filters.private & filters.command('set_rep'))
+async def add_rep(client, message):
+    if len(message.command) < 3:
+        return await message.reply_text("**__Give The Prefix__\n\nExᴀᴍᴩʟᴇ:- `/set_prefix @Roofiverse`**")
+    txt = message.text.split(" ", 2)
+    Sydd = txt[1]
+    Syddd = txt[2] if txt[2] else ''
+    SyD = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_rep(message.from_user.id, Sydd, Syddd)
+    await SyD.edit("__**ꜱᴀᴠᴇᴅ !**__")
+
+
+@Client.on_message(filters.private & filters.command('del_rep'))
+async def delete_rep(client, message):
+    SyD = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    dump = await db.get_rep(message.from_user.id)
+    if not dump:
+        return await SyD.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+    await db.set_rep(message.from_user.id, None, None)
+    await SyD.edit("__**❌️ ᴘʀᴇꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('set_topic'))
+async def add_topic(client, message):
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The ᴅᴜᴍᴩ ᴄʜᴀɴɴᴇʟ ɪᴅ__\n\nExᴀᴍᴩʟᴇ:- `/set_dump -1002042969565`**")
+    mrsyd = message.text.split(" ", 1)[1]
+   # mrsyd = await client.ask(message.from_user.id, "<b>ꜱᴇɴᴅ ᴍᴇ ᴛʜᴇ ᴛᴏᴩɪᴄ ɪᴅ ᴏʀ ʟɪɴᴋ.\n\n/cancel - ᴄᴀɴᴄᴇʟ ᴛʜɪs ᴘʀᴏᴄᴇss.</b>")
+    if mrsyd.startswith("https://t.me/"):
+        match = re.search(r"/(\d+)$", mrsyd)
+        if match:
+            topic_id = match.group(1)
+            txt = topic_id
+        else:
+            return await message.reply("<b>⚠ Invalid link provided. Make sure it ends with a numeric topic ID.</b>")
+    else:
+        txt = mrsyd.split(" ", 1)[0]
+    SyD = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_topic(message.from_user.id, txt)
+    await SyD.edit("__**✅ ᴛᴏᴩɪᴄ ꜱᴀᴠᴇᴅ**__")
