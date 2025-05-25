68aae7ba0487458b57471079cd0622a7869585eb
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-05-25 17:43:18 +0530

    Update metadata.py

--- plugins/metadata.py
+++ plugins/metadata.py
@@ -13,7 +13,7 @@ async def handle_metadata(bot: Client, message: Message):
     user_metadata = await db.get_metadata_code(message.from_user.id)
     markup = await features_button(message.from_user.id)
 
-    await ms.edit(f'**ʜᴇʀᴇ ᴛʜᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ғᴇᴀᴛᴜʀᴇ** 🍀**\n\nYour Current Metadata:-\n\n➜ `{user_metadata}` ', reply_markup=markup)
+    await ms.edit(f'**Hᴇʀᴇ ɪꜱ ᴛʜᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ғᴇᴀᴛᴜʀᴇ** 🍀**\n\nYᴏᴜʀ Cᴜʀʀᴇɴᴛ Mᴇᴛᴀᴅᴀᴛᴀ:-\n\n➜ `{user_metadata}` \nUꜱᴇ /set_metadata ᴛᴏ ᴄʜᴀɴɢᴇ ᴍᴇᴛᴀᴅᴀᴛᴀ 😔', reply_markup=markup)
 
 
 @Client.on_message(filters.private & filters.command('set_metadata'))
@@ -21,9 +21,9 @@ async def handle_set_metadata(bot: Client, message: Message):
     try:
         metadata = await bot.ask(text=Txt.SEND_METADATA, chat_id=message.from_user.id, filters=filters.text, timeout=30, disable_web_page_preview=True)
     except ListenerTimeout:
-        await message.reply_text("⚠️ Error!!\n\n**Request timed out.**\nRestart by using /metadata", reply_to_message_id=message.id)
+        await message.reply_text("⚠️ Error!!\n\n**Request timed out.**\nRestart by using /set_metadata", reply_to_message_id=message.id)
         return
     print(metadata.text)
     ms = await message.reply_text("**Please Wait...**", reply_to_message_id=metadata.id)
     await db.set_metadata_code(message.from_user.id, metadata_code=metadata.text)
-    await ms.edit("**Your Metadta Code Set Successfully ✅**")
+    await ms.edit("**Yᴏᴜʀ Mᴇᴛᴀᴅᴀᴛᴀ Cᴏᴅᴇ Sᴇᴛ Sᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ✅**")
