e6817bb867416da080c80e58d294d50cf39728de
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-06-09 07:41:41 +0530

    Update mrsyd.py

--- plugins/mrsyd.py
+++ plugins/mrsyd.py
@@ -1,4 +1,4 @@
-import math, time, random, os, tempfile, asyncioasyncio, re
+import math, time, random, os, tempfile, asyncio, re
 from pyrogram import Client, enums, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto
 from helper.database import db
