7e18112b93ac1f4474fa60d73f6fbfdd1e25c3c5
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-06-28 18:14:09 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -185,7 +185,7 @@ async def set_delete_handler(bot, message: Message):
         if not await is_user_admin(bot, message.from_user.id, message.chat.id):
             return await message.reply("❌ Only group admins can view auto-delete time.")
 
-        await db.col.update_one(
+        await db.chats.update_one(
             {"chat_id": message.chat.id},
             {"$set": {"delete_after": time_sec}},
             upsert=True
