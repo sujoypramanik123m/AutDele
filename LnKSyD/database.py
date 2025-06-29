8f7b700fb2ac4a96c873d239668e57c7f174c0e3
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-06-29 07:41:40 +0530

    Create database.py

---
+++ LnKSyD/database.py
@@ -0,0 +1,26 @@
+import motor.motor_asyncio
+from config import Config
+
+class Database:
+    def __init__(self, uri, database_name):
+        self.client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self.client[database_name]
+        self.users = self.db.users          # for storing users
+        self.chats = self.db.chats          # for storing group auto-delete time
+         # for userbot sessions
+
+    # ─── USER METHODS ────────────────────────────────────────────────────────────
+
+    async def is_user_exist(self, id):
+        user = await self.users.find_one({'_id': int(id)})
+        return bool(user)
+
+    async def add_user(self, user_id: int):
+        if not await self.users.find_one({"_id": user_id}):
+            await self.users.insert_one({"_id": user_id})
+
+    async def get_all_users(self):
+        return self.users.find({})
+
+    async def total_users_count(self):
+        return await self.users.count_documents({})
